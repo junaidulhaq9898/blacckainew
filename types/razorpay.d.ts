7862a23f6910b384c123a4f91b837ae010ccea29
junaid <junaid@Junaids-MacBook-Pro.local>
2025-03-13 00:32:34 +0530

    final p48

--- types/razorpay.d.ts
+++ types/razorpay.d.ts
@@ -1,16 +1,22 @@
 declare module 'razorpay' {
-    interface PaymentLinkCreateRequestBody {
-      subscription_id?: string;
+    interface RazorpayOptions {
+      key_id: string;
+      key_secret: string;
     }
   
-    interface PaymentLink {
-      short_url: string;
-    }
-  
-    interface Subscriptions {
-      notes?: {
-        userId: string;
-        [key: string]: unknown;
+    interface RazorpayInstance {
+      subscriptions: {
+        create: (options: any) => Promise<any>;
+        fetch: (subscriptionId: string) => Promise<any>;
+      };
+      paymentLink: {
+        create: (options: any) => Promise<any>;
       };
     }
+  
+    const Razorpay: {
+      new (options: RazorpayOptions): RazorpayInstance;
+    };
+    
+    export = Razorpay;
   }
